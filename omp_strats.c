bdc5ec7ee31a531b127ff2d0941dbbda78b91627
aarunishsinha <aarunish.sinha@gmail.com>
2021-04-14 22:30:58 +0530

    Strategy 2 (not working)

--- omp_strats.c
+++ omp_strats.c
@@ -35,40 +35,45 @@ void crout(double const **A, double **L, double **U, int n) {
 void s2_crout(double const **A, double **L, double **U, int n, int num_threads) {
 	int i, j, k;
 	double sum = 0;
-	omp_set_num_threads(num_threads);
+	// omp_set_num_threads(num_threads);
 	for (i = 0; i < n; i++) {
 		U[i][i] = 1;
 	}
 	for (j = 0; j < n; j++) {
-		#pragma omp sections
-		{
-			#pragma omp section
-			for (i = j+1; i < n; i++) {
-				sum = 0;
-				for (k = 0; k < j; k++) {
-					sum = sum + L[i][k] * U[k][j];
-				}
-				L[i][j] = A[i][j] - sum;
-			}
-			#pragma omp section
+		// #pragma omp parallel num_threads(num_threads)
+		// {
+			#pragma omp parallel sections num_threads(num_threads) private(sum)
 			{
-				sum = 0;
-				for (k = 0; k < j; k++) {
-					sum = sum + L[j][k] * U[k][j];
+				#pragma omp section
+				{
+					for (i = j+1; i < n; i++) {
+						sum = 0;
+						for (k = 0; k < j; k++) {
+							sum = sum + L[i][k] * U[k][j];
+						}
+						L[i][j] = A[i][j] - sum;
+					}
 				}
-				L[j][j] = A[j][j] - sum;
-				for (i = j; i < n; i++) {
+				#pragma omp section
+				{
 					sum = 0;
-					for(k = 0; k < j; k++) {
-						sum = sum + L[j][k] * U[k][i];
+					for (k = 0; k < j; k++) {
+						sum = sum + L[j][k] * U[k][j];
 					}
-					if (L[j][j] == 0) {
-						exit(0);
+					L[j][j] = A[j][j] - sum;
+					for (i = j; i < n; i++) {
+						sum = 0;
+						for(k = 0; k < j; k++) {
+							sum = sum + L[j][k] * U[k][i];
+						}
+						if (L[j][j] == 0) {
+							exit(0);
+						}
+						U[j][i] = (A[j][i] - sum) / L[j][j];
 					}
-					U[j][i] = (A[j][i] - sum) / L[j][j];
 				}
 			}
-		}
+		// }
 	}
 }
 void write_output(char fname[], double** arr, int n ){
