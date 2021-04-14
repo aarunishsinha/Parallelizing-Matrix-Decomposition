46afbe3d1931c1dfe4aeef4b16f530017ce0ce26
jaiarora0011 <jaiarora0011@gmail.com>
2021-04-15 04:50:48 +0530

    Strategy 2 Fixed

--- omp_strats.c
+++ omp_strats.c
@@ -33,51 +33,45 @@ void crout(double const **A, double **L, double **U, int n) {
 	}
 }
 void s2_crout(double const **A, double **L, double **U, int n, int num_threads) {
-	int i, j, k;
-	double sum = 0;
-	// omp_set_num_threads(num_threads);
-	for (i = 0; i < n; i++) {
-		U[i][i] = 1;
-	}
-	for (j = 0; j < n; j++) {
-		#pragma omp parallel num_threads(num_threads) private(sum)
-		{
-			#pragma omp sections private(sum)
-			{
-				#pragma omp section
-				{
-					for (i = j+1; i < n; i++) {
-						sum = 0;
-							for (k = 0; k < j; k++) {
-								sum = sum + L[i][k] * U[k][j];
-							}
-						L[i][j] = A[i][j] - sum;
-					}
-				}
-			}
-			#pragma omp sections private(sum)
-			{
-				#pragma omp section
-				{
-					sum = 0;
-						for (k = 0; k < j; k++) {
-							sum = sum + L[j][k] * U[k][j];
-						}
-					L[j][j] = A[j][j] - sum;
-					for (i = j; i < n; i++) {
-						sum = 0;
-							for(k = 0; k < j; k++) {
-								sum = sum + L[j][k] * U[k][i];
-							}
-						if (L[j][j] == 0) {
-							exit(0);
-						}
-						U[j][i] = (A[j][i] - sum) / L[j][j];
-					}
-				}
-			}
-		}
-	}
+    int i, j, k;
+    // omp_set_num_threads(num_threads);
+    double sum = 0;
+    for (i = 0; i < n; i++) {
+        U[i][i] = 1;
+    }
+    for (j = 0; j < n; j++) {
+        #pragma omp parallel sections num_threads(num_threads) private(i, k, sum)
+        {
+            #pragma omp section
+            {
+                for (i = j + 1; i < n; i++) {
+                    sum = 0;
+                    for (k = 0; k < j; k++) {
+                        sum = sum + L[i][k] * U[k][j];
+                    }
+                    L[i][j] = A[i][j] - sum;
+                }
+            }
+            #pragma omp section
+            {
+                sum = 0;
+                for (k = 0; k < j; k++) {
+                    sum = sum + L[j][k] * U[k][j];
+                }
+                L[j][j] = A[j][j] - sum;
+                for (i = j; i < n; i++) {
+                    sum = 0;
+                    for(k = 0; k < j; k++) {
+                        sum = sum + L[j][k] * U[k][i];
+                    }
+                    if (L[j][j] == 0) {
+                        exit(0);
+                    }
+                    U[j][i] = (A[j][i] - sum) / L[j][j];
+                }
+            }
+        }
+    }
 }
 void write_output(char fname[], double** arr, int n ){
 	FILE *f = fopen(fname, "w");
@@ -89,42 +83,45 @@ void write_output(char fname[], double** arr, int n ){
 	}
 	fclose(f);
 }
-int main(int argc, char* argv[]){
-	double start,end;
-	int n = atoi(argv[1]);
-	char* filename = argv[2];
-	int num_threads = atoi(argv[3]);
-	int strt = atoi(argv[4]);
-	FILE* f;
-	double **inp=(double**)malloc(n*(sizeof(double*)));
-	for(int i=0;i<n;i++){
-    inp[i]=(double*)malloc(sizeof(double)*n);
-	}
-	if((f = fopen(filename, "r")) == NULL)
-  	exit(1);
 
-  for(int jj=0; jj<n; jj++){
-    for(int ii=0; ii<n; ii++){
-      fscanf(f, "%lf", &inp[jj][ii]);
+int main(int argc, char* argv[]){
+	double start = 0, end = 0;
+    char* end_pointer;
+	int n = strtol(argv[1], &end_pointer, 10);
+	char* filename = argv[2];
+	int num_threads = strtol(argv[3], &end_pointer, 10);
+	int strt = strtol(argv[4], &end_pointer, 10);
+	FILE* f;
+	double **inp = (double**)malloc(n * (sizeof(double*)));
+	for(int i=0;i<n;i++){
+        inp[i] = (double*)malloc(sizeof(double) * n);
+	}
+
+    if((f = fopen(filename, "r")) == NULL)
+  	    exit(1);
+
+    for(int jj = 0; jj < n; jj++){
+        for(int ii = 0; ii<n; ii++){
+            fscanf(f, "%lf", &inp[jj][ii]);
 		}
 	}
-  fclose(f);
-	const double **A = (const double **) inp;
+    fclose(f);
+    const double **A = (const double **) inp;
 	// Printing the matrix
-  // for(int jj=0; jj<n; jj++){
-  //   for(int ii=0; ii<n; ii++)
-  //     printf ("%0.12f", A[jj][ii]);
-  //   printf("\n");
-  // }
-	double **L=(double**)malloc(n*(sizeof(double*)));
-	for(int i=0;i<n;i++){
-    L[i]=(double*)malloc(sizeof(double)*n);
+    // for(int jj=0; jj<n; jj++){
+    //   for(int ii=0; ii<n; ii++)
+    //     printf ("%0.12f", A[jj][ii]);
+    //   printf("\n");
+    // }
+	double **L = (double**)malloc(n * (sizeof(double*)));
+	for(int i = 0; i < n; i++){
+        L[i] = (double*)malloc(sizeof(double) * n);
 	}
-	double **U=(double**)malloc(n*(sizeof(double*)));
-	for(int i=0;i<n;i++){
-    U[i]=(double*)malloc(sizeof(double)*n);
+	double **U = (double**)malloc(n * (sizeof(double*)));
+	for(int i = 0; i < n; i++){
+        U[i] = (double*)malloc(sizeof(double)*n);
 	}
-	if(strt==0){
+    if(strt == 0){
 		start = omp_get_wtime();
 		crout(A,L,U,n);
 		end = omp_get_wtime();
@@ -142,7 +139,7 @@ int main(int argc, char* argv[]){
 		strncat(u_out_fname,ext,4);
 		write_output(u_out_fname,U,n);
 	}
-	else if(strt==2){
+    else if(strt == 2){
 		start = omp_get_wtime();
 		s2_crout(A,L,U,n,num_threads);
 		end = omp_get_wtime();
